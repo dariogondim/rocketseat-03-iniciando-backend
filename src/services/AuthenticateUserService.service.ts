1346a91e924f3107dccd694232de661b60d886f6
Dario gondim <cognusdev@gmail.com>
2020-12-21 15:13:00 -0300

    add sign token in service and response

--- src/services/AuthenticateUserService.service.ts
+++ src/services/AuthenticateUserService.service.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
     user: User;
+    token: string;
 }
 
 class AuthenticateUserService {
@@ -32,7 +34,12 @@ class AuthenticateUserService {
             throw new Error('Incorrect email/password combination');
         }
 
-        return { user };
+        const token = sign({}, '9fef3c4a34a03b86564f9f078989f481', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
+        return { user, token };
     }
 }
 
